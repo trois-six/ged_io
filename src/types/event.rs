62c49f1b2ddde71a2541d03e2c11840acc604c98
ge3224 <ge3224@gmail.com>
2025-06-06 22:00:33 -0500

    refactor: event mod tests

--- src/types/event.rs
+++ src/types/event.rs
@@ -64,41 +64,6 @@ impl ToString for Event {
 /// EVEN tag in this structure is for recording general events that are not specified in the
 /// specification. The event indicated by this general EVEN tag is defined by the value of the
 /// subordinate TYPE tag (event_type).
-///
-/// # Example
-///
-/// ```rust
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///    0 HEAD\n\
-///    1 GEDC\n\
-///    2 VERS 5.5\n\
-///    0 @PERSON1@ INDI
-///    1 CENS\n\
-///    2 DATE 31 DEC 1997\n\
-///    2 PLAC The place\n\
-///    2 SOUR @SOURCE1@\n\
-///    3 PAGE 42\n\
-///    3 DATA\n\
-///    4 DATE 31 DEC 1900\n\
-///    4 TEXT a sample text\n\
-///    5 CONT Sample text continued here. The word TE\n\
-///    5 CONC ST should not be broken!\n\
-///    3 QUAY 3\n\
-///    3 NOTE A note\n\
-///    4 CONT Note continued here. The word TE\n\
-///    4 CONC ST should not be broken!\n\
-///    2 NOTE CENSUS event note (the event of the periodic count of the population for a designated locality, such as a national or state Census)\n\
-///    3 CONT Note continued here. The word TE\n\
-///    3 CONC ST should not be broken!\n\
-///    0 TRLR";
-///
-/// let mut doc = GedcomDocument::new(sample.chars());
-/// let data = doc.parse_document();
-///
-/// let event = data.individuals[0].events[0].event.to_string();
-/// assert_eq!(event, "Census");
-/// ```
 #[derive(Clone)]
 #[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
 pub struct EventDetail {
@@ -291,46 +256,6 @@ impl ToString for Spouse {
 }
 
 /// FamilyEventDetail defines an additional dataset found in certain events.
-///
-/// # Example
-///
-/// ```rust
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///    0 HEAD\n\
-///    1 GEDC\n\
-///    2 VERS 5.5\n\
-///    0 @FAMILY1@ FAM
-///    1 ANUL
-///    2 DATE 31 DEC 1997
-///    2 PLAC The place
-///    2 SOUR @SOURCE1@
-///    3 PAGE 42
-///    3 DATA
-///    4 DATE 31 DEC 1900
-///    4 TEXT a sample text
-///    5 CONT Sample text continued here. The word TE
-///    5 CONC ST should not be broken!
-///    3 QUAY 3
-///    3 NOTE A note
-///    4 CONT Note continued here. The word TE
-///    4 CONC ST should not be broken!
-///    2 NOTE ANNULMENT event note (declaring a marriage void from the beginning (never existed))
-///    3 CONT Note continued here. The word TE
-///    3 CONC ST should not be broken!
-///    2 HUSB
-///    3 AGE 42y
-///    2 WIFE
-///    3 AGE 42y 6m
-///    0 TRLR";
-///
-/// let mut doc = GedcomDocument::new(sample.chars());
-/// let data = doc.parse_document();
-///
-/// let anul = &data.families[0].events;
-/// assert_eq!(anul.len(), 1);
-///
-/// ```
 #[derive(Clone)]
 pub struct FamilyEventDetail {
     pub member: Spouse,
@@ -370,5 +295,81 @@ impl Parser for FamilyEventDetail {
             ),
         };
         parse_subset(tokenizer, level, handle_subset);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::GedcomDocument;
+
+    #[test]
+    fn test_parse_person_event() {
+        let sample = "\
+           0 HEAD\n\
+           1 GEDC\n\
+           2 VERS 5.5\n\
+           0 @PERSON1@ INDI
+           1 CENS\n\
+           2 DATE 31 DEC 1997\n\
+           2 PLAC The place\n\
+           2 SOUR @SOURCE1@\n\
+           3 PAGE 42\n\
+           3 DATA\n\
+           4 DATE 31 DEC 1900\n\
+           4 TEXT a sample text\n\
+           5 CONT Sample text continued here. The word TE\n\
+           5 CONC ST should not be broken!\n\
+           3 QUAY 3\n\
+           3 NOTE A note\n\
+           4 CONT Note continued here. The word TE\n\
+           4 CONC ST should not be broken!\n\
+           2 NOTE CENSUS event note (the event of the periodic count of the population for a designated locality, such as a national or state Census)\n\
+           3 CONT Note continued here. The word TE\n\
+           3 CONC ST should not be broken!\n\
+           0 TRLR";
+
+        let mut doc = GedcomDocument::new(sample.chars());
+        let data = doc.parse_document();
+
+        let event = data.individuals[0].events[0].event.to_string();
+        assert_eq!(event, "Census");
+    }
+
+    #[test]
+    fn test_parse_family_event() {
+        let sample = "\
+           0 HEAD\n\
+           1 GEDC\n\
+           2 VERS 5.5\n\
+           0 @FAMILY1@ FAM
+           1 ANUL
+           2 DATE 31 DEC 1997
+           2 PLAC The place
+           2 SOUR @SOURCE1@
+           3 PAGE 42
+           3 DATA
+           4 DATE 31 DEC 1900
+           4 TEXT a sample text
+           5 CONT Sample text continued here. The word TE
+           5 CONC ST should not be broken!
+           3 QUAY 3
+           3 NOTE A note
+           4 CONT Note continued here. The word TE
+           4 CONC ST should not be broken!
+           2 NOTE ANNULMENT event note (declaring a marriage void from the beginning (never existed))
+           3 CONT Note continued here. The word TE
+           3 CONC ST should not be broken!
+           2 HUSB
+           3 AGE 42y
+           2 WIFE
+           3 AGE 42y 6m
+           0 TRLR";
+
+        let mut doc = GedcomDocument::new(sample.chars());
+        let data = doc.parse_document();
+
+        let anul = &data.families[0].events;
+        assert_eq!(anul.len(), 1);
     }
 }
