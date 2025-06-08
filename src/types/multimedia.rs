7b21ae51831ef04ca1dca7f8d33c80be9871532a
ge3224 <ge3224@gmail.com>
2025-06-07 22:02:25 -0500

    refactor: multimedia mod tests

--- src/types/multimedia.rs
+++ src/types/multimedia.rs
@@ -15,42 +15,12 @@ use crate::{
 /// The change and creation dates should be for the OBJE record itself, not the underlying files.
 ///
 /// See https://gedcom.io/specifications/FamilySearchGEDCOMv7.html#MULTIMEDIA_RECORD.
-///
-/// # Example
-/// ```
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///     0 HEAD\n\
-///     1 GEDC\n\
-///     2 VERS 5.5\n\
-///     2 FORM LINEAGE-LINKED\n\
-///     0 @MEDIA1@ OBJE\n\
-///     1 FILE /home/user/media/file_name.bmp\n\
-///     1 TITL A Title\n\
-///     1 RIN Automated Id\n\
-///     0 TRLR";
-///
-/// let mut doc = GedcomDocument::new(sample.chars());
-/// let data = doc.parse_document();
-///
-/// assert_eq!(data.multimedia.len(), 1);
-/// let obje = &data.multimedia[0];
-///
-/// let xref = obje.xref.as_ref().unwrap();
-/// assert_eq!(xref, "@MEDIA1@");
-///
-/// let titl = obje.title.as_ref().unwrap();
-/// assert_eq!(titl, "A Title");
-///
-/// let rin = obje.automated_record_id.as_ref().unwrap();
-/// assert_eq!(rin, "Automated Id");
-/// ```
 #[derive(Clone, Debug, Default)]
 #[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
 pub struct MultimediaRecord {
     /// Optional reference to link to this submitter
     pub xref: Option<Xref>,
-    pub file: Option<MultimediaFileRefn>,
+    pub file: Option<MultimediaFileRef>,
     /// The 5.5 spec, page 26, shows FORM as a sub-structure of FILE, but the struct appears as a
     /// sibling in an Ancestry.com export.
     pub form: Option<MultimediaFormat>,
@@ -80,7 +50,7 @@ impl Parser for MultimediaRecord {
         tokenizer.next_token();
 
         let handle_subset = |tag: &str, tokenizer: &mut Tokenizer| match tag {
-            "FILE" => self.file = Some(MultimediaFileRefn::new(tokenizer, level + 1)),
+            "FILE" => self.file = Some(MultimediaFileRef::new(tokenizer, level + 1)),
             "FORM" => self.form = Some(MultimediaFormat::new(tokenizer, level + 1)),
             "TITL" => self.title = Some(tokenizer.take_line_value()),
             "REFN" => {
@@ -97,48 +67,12 @@ impl Parser for MultimediaRecord {
 }
 
 /// MultimediaLink... TODO
-///
-/// # Example
-/// ```
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///     0 HEAD\n\
-///     1 CHAR UTF-8\n\
-///     1 SOUR Ancestry.com Family Trees\n\
-///     2 VERS (2010.3)\n\
-///     2 NAME Ancestry.com Family Trees\n\
-///     2 CORP Ancestry.com\n\
-///     1 GEDC\n\
-///     2 VERS 5.5\n\
-///     2 FORM LINEAGE-LINKED\n\
-///     0 OBJE\n\
-///     1 FILE http://trees.ancestry.com/rd?f=image&guid=Xxxxxxxx-Xxxx-Xxxx-Xxxx-Xxxxxxxxxxxx&tid=Xxxxxxxx&pid=1\n\
-///     1 FORM jpg\n\
-///     1 TITL In Prague\n\
-///     0 TRLR";
-///
-/// let mut record = GedcomDocument::new(sample.chars());
-/// let data = record.parse_document();
-/// assert_eq!(data.multimedia.len(), 1);
-///
-/// let obje = &data.multimedia[0];
-/// assert_eq!(obje.title.as_ref().unwrap(), "In Prague");
-///
-/// let form = obje.form.as_ref().unwrap();
-/// assert_eq!(form.value.as_ref().unwrap(), "jpg");
-///
-/// let file = obje.file.as_ref().unwrap();
-/// assert_eq!(
-///     file.value.as_ref().unwrap(),
-///     "http://trees.ancestry.com/rd?f=image&guid=Xxxxxxxx-Xxxx-Xxxx-Xxxx-Xxxxxxxxxxxx&tid=Xxxxxxxx&pid=1"
-/// );
-/// ```
 #[derive(Debug, Default)]
 #[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
 pub struct MultimediaLink {
     /// Optional reference to link to this submitter
     pub xref: Option<Xref>,
-    pub file: Option<MultimediaFileRefn>,
+    pub file: Option<MultimediaFileRef>,
     /// The 5.5 spec, page 26, shows FORM as a sub-structure of FILE, but the struct appears as a
     /// sibling in an Ancestry.com export.
     pub form: Option<MultimediaFormat>,
@@ -167,7 +101,7 @@ impl Parser for MultimediaLink {
         tokenizer.next_token();
 
         let handle_subset = |tag: &str, tokenizer: &mut Tokenizer| match tag {
-            "FILE" => self.file = Some(MultimediaFileRefn::new(tokenizer, level + 1)),
+            "FILE" => self.file = Some(MultimediaFileRef::new(tokenizer, level + 1)),
             "FORM" => self.form = Some(MultimediaFormat::new(tokenizer, level + 1)),
             "TITL" => self.title = Some(tokenizer.take_line_value()),
             _ => panic!("{} Unhandled Multimedia Tag: {}", tokenizer.debug(), tag),
@@ -176,59 +110,27 @@ impl Parser for MultimediaLink {
     }
 }
 
-/// MultimediaFileRefn is a complete local or remote file reference to the auxiliary data to be
+/// MultimediaFileRef is a complete local or remote file reference to the auxiliary data to be
 /// linked to the GEDCOM context. Remote reference would include a network address where the
 /// multimedia data may be obtained.
-///
-/// # Example
-///
-/// ```rust
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///     0 HEAD\n\
-///     1 GEDC\n\
-///     2 VERS 5.5\n\
-///     0 @MEDIA1@ OBJE\n\
-///     1 FILE /home/user/media/file_name.bmp\n\
-///     2 FORM bmp\n\
-///     3 TYPE photo
-///     2 TITL A Bitmap\n\
-///     0 TRLR";
-///
-/// let mut doc = GedcomDocument::new(sample.chars());
-/// let data = doc.parse_document();
-/// assert_eq!(data.multimedia.len(), 1);
-///
-/// let file = data.multimedia[0].file.as_ref().unwrap();
-/// assert_eq!(
-///     file.value.as_ref().unwrap(),
-///     "/home/user/media/file_name.bmp"
-/// );
-///
-/// assert_eq!(file.title.as_ref().unwrap(), "A Bitmap");
-///
-/// let form = file.form.as_ref().unwrap();
-/// assert_eq!(form.value.as_ref().unwrap(), "bmp");
-/// assert_eq!(form.source_media_type.as_ref().unwrap(), "photo");
-/// ```
 #[derive(Clone, Debug, Default)]
 #[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
-pub struct MultimediaFileRefn {
+pub struct MultimediaFileRef {
     pub value: Option<String>,
     pub title: Option<String>,
     pub form: Option<MultimediaFormat>,
 }
 
-impl MultimediaFileRefn {
+impl MultimediaFileRef {
     #[must_use]
-    pub fn new(tokenizer: &mut Tokenizer, level: u8) -> MultimediaFileRefn {
-        let mut file = MultimediaFileRefn::default();
+    pub fn new(tokenizer: &mut Tokenizer, level: u8) -> MultimediaFileRef {
+        let mut file = MultimediaFileRef::default();
         file.parse(tokenizer, level);
         file
     }
 }
 
-impl Parser for MultimediaFileRefn {
+impl Parser for MultimediaFileRef {
     fn parse(&mut self, tokenizer: &mut Tokenizer, level: u8) {
         self.value = Some(tokenizer.take_line_value());
         let handle_subset = |tag: &str, tokenizer: &mut Tokenizer| match tag {
@@ -251,31 +153,6 @@ impl Parser for MultimediaFileRefn {
 ///
 /// NOTE: The 5.5 spec lists the following seven formats [ bmp | gif | jpg | ole | pcx | tif | wav ].
 /// However, we're leaving this open for emerging formats, Option<String>.
-///
-/// # Example
-///
-/// ```rust
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///     0 HEAD\n\
-///     1 GEDC\n\
-///     2 VERS 5.5\n\
-///     0 @MEDIA1@ OBJE\n\
-///     1 FILE /home/user/media/file_name.bmp\n\
-///     2 FORM bmp\n\
-///     3 TYPE photo
-///     0 TRLR";
-///
-/// let mut doc = GedcomDocument::new(sample.chars());
-/// let data = doc.parse_document();
-/// assert_eq!(data.multimedia.len(), 1);
-///
-/// let file = data.multimedia[0].file.as_ref().unwrap();
-///
-/// let form = file.form.as_ref().unwrap();
-/// assert_eq!(form.value.as_ref().unwrap(), "bmp");
-/// assert_eq!(form.source_media_type.as_ref().unwrap(), "photo");
-/// ```
 #[derive(Clone, Debug, Default)]
 #[cfg_attr(feature = "json", derive(Serialize, Deserialize))]
 pub struct MultimediaFormat {
@@ -314,33 +191,6 @@ impl Parser for MultimediaFormat {
 /// UserReferenceNumber is a user-defined number or text that the submitter uses to identify this
 /// record. For instance, it may be a record number within the submitter's automated or manual
 /// system, or it may be a page and position number on a pedigree chart.
-///
-/// # Example
-///
-/// ```rust
-/// use gedcom::GedcomDocument;
-/// let sample = "\
-///     0 HEAD\n\
-///     1 GEDC\n\
-///     2 VERS 5.5\n\
-///     2 FORM LINEAGE-LINKED\n\
-///     0 @MEDIA1@ OBJE\n\
-///     1 FILE /home/user/media/file_name.bmp\n\
-///     1 REFN 000\n\
-///     2 TYPE User Reference Type\n\
-///     0 TRLR";
-///
-/// let mut doc = GedcomDocument::new(sample.chars());
-/// let data = doc.parse_document();
-/// assert_eq!(data.multimedia.len(), 1);
-///
-/// let user_ref = data.multimedia[0].user_reference_number.as_ref().unwrap();
-/// assert_eq!(user_ref.value.as_ref().unwrap(), "000");
-/// assert_eq!(
-///     user_ref.user_reference_type.as_ref().unwrap(),
-///     "User Reference Type"
-/// );
-/// ```
 #[derive(Clone)]
 pub struct UserReferenceNumber {
     /// line value
@@ -371,5 +221,149 @@ impl Parser for UserReferenceNumber {
             ),
         };
         parse_subset(tokenizer, level, handle_subset);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::GedcomDocument;
+
+    #[test]
+    fn test_parse_multimedia_record() {
+        let sample = "\
+            0 HEAD\n\
+            1 GEDC\n\
+            2 VERS 5.5\n\
+            2 FORM LINEAGE-LINKED\n\
+            0 @MEDIA1@ OBJE\n\
+            1 FILE /home/user/media/file_name.bmp\n\
+            1 TITL A Title\n\
+            1 RIN Automated Id\n\
+            0 TRLR";
+
+        let mut doc = GedcomDocument::new(sample.chars());
+        let data = doc.parse_document();
+
+        assert_eq!(data.multimedia.len(), 1);
+        let obje = &data.multimedia[0];
+
+        let xref = obje.xref.as_ref().unwrap();
+        assert_eq!(xref, "@MEDIA1@");
+
+        let titl = obje.title.as_ref().unwrap();
+        assert_eq!(titl, "A Title");
+
+        let rin = obje.automated_record_id.as_ref().unwrap();
+        assert_eq!(rin, "Automated Id");
+    }
+
+    #[test]
+    fn test_parse_multimedia_link() {
+        let sample = "\
+            0 HEAD\n\
+            1 CHAR UTF-8\n\
+            1 SOUR Ancestry.com Family Trees\n\
+            2 VERS (2010.3)\n\
+            2 NAME Ancestry.com Family Trees\n\
+            2 CORP Ancestry.com\n\
+            1 GEDC\n\
+            2 VERS 5.5\n\
+            2 FORM LINEAGE-LINKED\n\
+            0 OBJE\n\
+            1 FILE http://trees.ancestry.com/rd?f=image&guid=Xxxxxxxx-Xxxx-Xxxx-Xxxx-Xxxxxxxxxxxx&tid=Xxxxxxxx&pid=1\n\
+            1 FORM jpg\n\
+            1 TITL In Prague\n\
+            0 TRLR";
+
+        let mut record = GedcomDocument::new(sample.chars());
+        let data = record.parse_document();
+        assert_eq!(data.multimedia.len(), 1);
+
+        let obje = &data.multimedia[0];
+        assert_eq!(obje.title.as_ref().unwrap(), "In Prague");
+
+        let form = obje.form.as_ref().unwrap();
+        assert_eq!(form.value.as_ref().unwrap(), "jpg");
+
+        let file = obje.file.as_ref().unwrap();
+        assert_eq!(file.value.as_ref().unwrap(), "http://trees.ancestry.com/rd?f=image&guid=Xxxxxxxx-Xxxx-Xxxx-Xxxx-Xxxxxxxxxxxx&tid=Xxxxxxxx&pid=1");
+    }
+
+    #[test]
+    fn test_parse_multimedia_file_ref_record() {
+        let sample = "\
+             0 HEAD\n\
+             1 GEDC\n\
+             2 VERS 5.5\n\
+             0 @MEDIA1@ OBJE\n\
+             1 FILE /home/user/media/file_name.bmp\n\
+             2 FORM bmp\n\
+             3 TYPE photo
+             2 TITL A Bitmap\n\
+             0 TRLR";
+
+        let mut doc = GedcomDocument::new(sample.chars());
+        let data = doc.parse_document();
+        assert_eq!(data.multimedia.len(), 1);
+
+        let file = data.multimedia[0].file.as_ref().unwrap();
+        assert_eq!(
+            file.value.as_ref().unwrap(),
+            "/home/user/media/file_name.bmp"
+        );
+
+        assert_eq!(file.title.as_ref().unwrap(), "A Bitmap");
+
+        let form = file.form.as_ref().unwrap();
+        assert_eq!(form.value.as_ref().unwrap(), "bmp");
+        assert_eq!(form.source_media_type.as_ref().unwrap(), "photo");
+    }
+
+    #[test]
+    fn test_parse_multimedia_format_record() {
+        let sample = "\
+            0 HEAD\n\
+            1 GEDC\n\
+            2 VERS 5.5\n\
+            0 @MEDIA1@ OBJE\n\
+            1 FILE /home/user/media/file_name.bmp\n\
+            2 FORM bmp\n\
+            3 TYPE photo
+            0 TRLR";
+
+        let mut doc = GedcomDocument::new(sample.chars());
+        let data = doc.parse_document();
+        assert_eq!(data.multimedia.len(), 1);
+
+        let file = data.multimedia[0].file.as_ref().unwrap();
+
+        let form = file.form.as_ref().unwrap();
+        assert_eq!(form.value.as_ref().unwrap(), "bmp");
+        assert_eq!(form.source_media_type.as_ref().unwrap(), "photo");
+    }
+
+    #[test]
+    fn test_parse_user_reference_number_record() {
+        let sample = "\
+            0 HEAD\n\
+            1 GEDC\n\
+            2 VERS 5.5\n\
+            2 FORM LINEAGE-LINKED\n\
+            0 @MEDIA1@ OBJE\n\
+            1 FILE /home/user/media/file_name.bmp\n\
+            1 REFN 000\n\
+            2 TYPE User Reference Type\n\
+            0 TRLR";
+
+        let mut doc = GedcomDocument::new(sample.chars());
+        let data = doc.parse_document();
+        assert_eq!(data.multimedia.len(), 1);
+
+        let user_ref = data.multimedia[0].user_reference_number.as_ref().unwrap();
+        assert_eq!(user_ref.value.as_ref().unwrap(), "000");
+        assert_eq!(
+            user_ref.user_reference_type.as_ref().unwrap(),
+            "User Reference Type"
+        );
     }
 }
